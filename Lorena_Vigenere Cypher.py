2f4e9aacda60fc39f26a4799483f30f1a6288de0
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-22 00:34:01 +0800

    Added for loop

--- Lorena_Vigenere Cypher.py
+++ Lorena_Vigenere Cypher.py
@@ -2,3 +2,8 @@ def encr(message, key):
     klen = len(key)
     kord = [ord(i) for i in key]
     mesord = [ord(i) for i in message]
+    enc = ''
+        for i in range(len(mesord)):
+        x = (mesord[i] + kord[i % klen]) % 26
+        enc += chr(x + 65)
+    return enc
